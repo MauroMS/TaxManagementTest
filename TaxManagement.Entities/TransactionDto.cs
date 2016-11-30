75cd8381c212c42d473a9ceae64531b0dba41943
MauroMS <mauro.schaparini@gmail.com>
2016-11-30 10:23:10 +0000

    DataAnnotations
    Added DataAnnotations for transactions

--- TaxManagement.Entities/TransactionDto.cs
+++ TaxManagement.Entities/TransactionDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +10,14 @@ namespace TaxManagement.Entities
     public class TransactionDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please provide an Account.")]
         public string Account { get; set; }
+        [Required(ErrorMessage = "Please provide a Description.")]
         public string Description { get; set; }
+        [StringLength(maximumLength: 3, MinimumLength = 3)]
+        [Required(ErrorMessage = "Please provide a Currency Code.")]
         public string CurrencyCode { get; set; }
+        [Required(ErrorMessage = "Please provide an Amount.")]
         public decimal Amount { get; set; }
         public string Message { get; set; }
 
